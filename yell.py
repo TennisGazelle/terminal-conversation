b9707852fd537fc255b47c98cb1e415e0e746fad
Daniel Lopez <daniellopez@nevada.unr.edu>
2019-12-08 14:37:50 -0800

    centered better; variable fonts as third col now

--- yell.py
+++ yell.py
@@ -6,11 +6,14 @@ import subprocess
 with open('test.csv', 'r') as f:
     lines = csv.reader(f)
 
-    for row in lines:
-        if row[0]:
-            subprocess.call('figlet -c -k {}'.format(row[0]).split())
-        if row[1] and row[1] is not '':
-            time.sleep(float(row[1]))
-        subprocess.call(['clear'])
+    default_font = 'standard'
 
-        
+    for row in lines:
+        font = default_font
+        if len(row) > 2 and row[2] is not '':
+            font = row[2]
+        if row[0]:
+            subprocess.call('figlet -w 120 -ckf {} {}'.format(font, row[0]).split())
+        if row[1] is not '':
+            time.sleep(float(row[1]))
+        subprocess.call(['clear'])        
